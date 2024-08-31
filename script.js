9d2cf55b977f96e7aa98a13d7f734115d71b778c
Ckyever Gaviola <ckyeverg@gmail.com>
2024-08-31 23:48:32 +1000

    Add basic page elements and start Spotify API request logic

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const clientId = "123";
+const clientSecret = "abc";
+
+var request = new XMLHttpRequest();
+request.open("POST", "https://accounts.spotify.com/api/token");
+request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+const requestBody = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+request.send(requestBody);
+
+const spotifyPlaylistUrlInput = document.getElementById("spotify-playlist-url");
+const convertButton = document.getElementById("convert-button");
+
+function convert() {
+    const spotifyUrl = document.getElementById('spotify-playlist-url').value;
+    console.log(spotifyUrl);
+}
+
+convertButton.addEventListener("click", convert);
+
+spotifyPlaylistUrlInput.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        convertButton.click();
+    }
+})
