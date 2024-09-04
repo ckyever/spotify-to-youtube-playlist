960da2d6c9b50c37801272721af4d3a1882a7a7e
Ckyever Gaviola <ckyeverg@gmail.com>
2024-09-04 21:09:45 +1000

    Retrieve artist data from Spotify API

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const clientId = "123";
 const clientSecret = "abc";
-
-var request = new XMLHttpRequest();
-request.open("POST", "https://accounts.spotify.com/api/token");
-request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 const requestBody = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
-request.send(requestBody);
+
+const url = "https://accounts.spotify.com/api/token";
+fetch(url, {
+    method: 'POST',
+    body: requestBody,
+    headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded'
+    })
+})
+.then(response => response.json())
+.then(json => {
+    console.log(json.access_token);
+    getArtistData(json.access_token);
+})
+
+function getArtistData(token) {
+    const url = "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb";
+    fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            'Authorization': `Bearer ${token}`
+        })
+    })
+    .then(response => response.json)
+    .then(json => console.log(JSON.stringify(json)))
+}
 
 const spotifyPlaylistUrlInput = document.getElementById("spotify-playlist-url");
 const convertButton = document.getElementById("convert-button");
