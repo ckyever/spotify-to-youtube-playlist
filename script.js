7f2083c670ce00a322f6531f859fb017bc7581c7
Ckyever Gaviola <ckyeverg@gmail.com>
2024-09-07 20:45:29 +1000

    Get array of song titles from playlist URL

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const getToken = async () => {
-    const tokenUrl = "https://accounts.spotify.com/api/token";
+    const url = "https://accounts.spotify.com/api/token";
     const clientId = "123";
     const clientSecret = "abc";
     const requestBody = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
 
     try {
-        const response = await fetch(tokenUrl, {
+        const response = await fetch(url, {
             method: 'POST',
             body: requestBody,
             headers: new Headers({
@@ -14,24 +14,37 @@ const getToken = async () => {
         });
         const data = await response.json();
         const {access_token} = await data;
-        console.log("Token: ", access_token);
         return access_token;
     } catch (error) {
-        console.log("Error occured: ", error);
+        console.log(error);
         return null;
     }
 }
 
-const getArtistData = async token => {
-    const url = "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb";
-    fetch(url, {
-        method: 'GET',
-        headers: new Headers({
-            'Authorization': `Bearer ${token}`
-        })
-    })
-    .then(response => response.json)
-    .then(json => console.log("Artist JSON", JSON.stringify(json)))
+const getPlaylist = async (token, playlistUrl) => {
+    playlistUrl = playlistUrl.slice(-1) === '/' ? playlistUrl.slice(0, -1) : playlistUrl;
+    const playlistId = playlistUrl.substring(playlistUrl.lastIndexOf('/') + 1);
+
+    try {
+        const url = `https://api.spotify.com/v1/playlists/${playlistId}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': `Bearer ${token}`
+            })
+        });
+        const data = await response.json();
+        const tracks = data.tracks.items;
+        const songTitles = tracks.map((item) => {
+            const mainArtist = item.track.artists[0].name;
+            const trackItem = item.track.name;
+            return `${mainArtist} - ${trackItem}`
+        });
+        return songTitles;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
 const spotifyPlaylistUrlInput = document.getElementById("spotify-playlist-url");
@@ -39,9 +52,8 @@ const convertButton = document.getElementById("convert-button");
 
 const convert = async () => {
     const spotifyUrl = document.getElementById('spotify-playlist-url').value;
-    console.log(spotifyUrl);
     const access_token = await getToken(); 
-    await getArtistData(access_token);
+    await getPlaylist(access_token, spotifyUrl);
 }
 
 convertButton.addEventListener("click", convert);
